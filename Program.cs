5cfca996dc27fa3ea1d5c7e229e10ac9a5c9686b
Yazan <yazan.makhoul03@gmail.com>
2026-08-27 15:47:06 +0300

    fix: handle division by zero and invalid input

--- Program.cs
+++ Program.cs
@@ -1,37 +1,41 @@
 ﻿while (true)
 {
-    Console.WriteLine("Choose +  -  /  *  ");
-    char operation = Convert.ToChar(Console.ReadLine());
-
-    Console.WriteLine("Enter First Number :");
-    int num1 = Convert.ToInt32(Console.ReadLine());
-
-    Console.WriteLine("Enter Second Number :");
-    int num2 = Convert.ToInt32(Console.ReadLine());
-
-    double result;
-
-    switch (operation)
+    try
     {
-        case '+':
-            result = num1 + num2;
-            Console.WriteLine($"Result = {result}");
-            break;
-        case '-':
-            result = num1 - num2;
-            Console.WriteLine($"Result = {result}");
-            break;
-        case '/':
-            result = num1 / num2;
-            Console.WriteLine($"Result = {result}");
-            break;
-        case '*':
-            result = num1 * num2;
-            Console.WriteLine($"Result = {result}");
-            break;
-        default:
-            Console.WriteLine("Invalid Operator");
-            break;
+        Console.WriteLine("Choose +  -  /  *  ");
+        char operation = Convert.ToChar(Console.ReadLine());
+        Console.WriteLine("Enter First Number :");
+        int num1 = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Enter Second Number :");
+        int num2 = Convert.ToInt32(Console.ReadLine());
+        double result;
+
+        switch (operation)
+        {
+            case '+':
+                result = num1 + num2;
+                Console.WriteLine($"Result = {result}");
+                break;
+            case '-':
+                result = num1 - num2;
+                Console.WriteLine($"Result = {result}");
+                break;
+            case '/':
+                result = num1 / num2;
+                Console.WriteLine($"Result = {result}");
+                break;
+            case '*':
+                result = num1 * num2;
+                Console.WriteLine($"Result = {result}");
+                break;
+            default:
+                Console.WriteLine("Invalid Operator");
+                break;
+        }
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine($"Error: {ex.Message}");
     }
 
     Console.WriteLine("You want to exit?");
